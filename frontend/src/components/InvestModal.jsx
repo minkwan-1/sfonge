24129cf4fa6a4cebd6440b39f6e457ec1130e1cc
원민관 <mkwan5741@gmail.com>
2025-11-30 15:34:33 +0900

    chore: 프로젝트 폴더 구조 및 기본 파일 생성

---
+++ frontend/src/components/InvestModal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InvestModal = () => {
+  return <div></div>;
+};
+
+export default InvestModal;
---
+++ frontend/src/components/NewProjectModal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewProjectModal = () => {
+  return <div></div>;
+};
+
+export default NewProjectModal;
